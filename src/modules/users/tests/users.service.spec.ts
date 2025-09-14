8774194342039f6307350f4c5353a7967c0f672f
Murilo Damario <murilo.gcdamario@gmail.com>
2025-09-13 23:04:06 -0300

    :sparkles: feat: Implements user tests

--- src/modules/users/tests/users.service.spec.ts
+++ src/modules/users/tests/users.service.spec.ts
@@ -1,6 +1,9 @@
-import { InMemoryUserRespository } from 'test/repositories/in-memory-user-repository'
+import { UserEmailAlreadyExistsError } from '@/commons/errors/application-errors/user-email-already-exists'
+
 import { UsersService } from '../users.service'
+
 import { makeUser } from 'test/factories/make-user'
+import { InMemoryUserRespository } from 'test/repositories/in-memory-user-repository'
 
 let usersService: UsersService
 let inMemoryUserRepository: InMemoryUserRespository
@@ -22,5 +25,23 @@ describe('Users Service Test Suit', () => {
     expect(response.isSuccess()).toBeTruthy()
     expect(inMemoryUserRepository.users).toHaveLength(1)
     expect(inMemoryUserRepository.users[0].name).toEqual('John Doe')
+  })
+
+  it('should not be able to create a new user with registered email', async () => {
+    const user = makeUser({
+      email: 'john@gmail'
+    })
+
+    inMemoryUserRepository.users.push(user)
+
+    const response = await usersService.create({
+      name: 'John Doe',
+      email: 'john@gmail',
+      password: '123456',
+      bio: 'bio'
+    })
+
+    expect(response.isFailure()).toBeTruthy()
+    expect(response.value).toBeInstanceOf(UserEmailAlreadyExistsError)
   })
 })
