aa597576140de077ba6dfa3d827b2ed3d8c623a6
Murilo Damario <murilo.gcdamario@gmail.com>
2025-09-15 22:20:19 -0300

    :sparkles: feat: Applies hash to password

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
+import { CryptographyModule } from '../cryptography/cryptography.module'
+
 import { User } from './entities/user.entity'
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { IUserRepository } from './repositories/user-repository.interface';
-import { TypeOrmUserRepository } from './repositories/type-orm-user-repository';
+import { UsersService } from './users.service'
+import { UsersController } from './users.controller'
+import { IUserRepository } from './repositories/user-repository.interface'
+import { TypeOrmUserRepository } from './repositories/type-orm-user-repository'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    CryptographyModule
+  ],
   providers: [
     UsersService,
     {
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common'
 
-import { UserEmailAlreadyExistsError } from 'src/commons/errors/application-errors/user-email-already-exists'
-import { failure, success, } from 'src/shared/either'
-import { CreateUserRequest, CreateUserResponse } from './dto/create-user.dto'
+import { UserEmailAlreadyExistsError } from '@/commons/errors/application-errors/user-email-already-exists'
+
 import { IUserRepository } from './repositories/user-repository.interface'
+
+import { HashGenerator } from '../cryptography/hash-generator'
+
+import { User } from './entities/user.entity'
+import { CreateUserRequest, CreateUserResponse } from './dto/create-user.dto'
+
+import { failure, success, } from 'src/shared/either'
 
 
 @Injectable()
 export class UsersService {
   constructor(
-    private readonly userRepository: IUserRepository
+    private readonly userRepository: IUserRepository,
+    private readonly hashGenerator: HashGenerator
   ) { }
 
   async create(data: CreateUserRequest): Promise<CreateUserResponse> {
@@ -19,8 +26,21 @@ export class UsersService {
       return failure(new UserEmailAlreadyExistsError())
     }
 
-    const user = await this.userRepository.save(data)
+    const hashedPassword = await this.hashGenerator.hash(data.password)
+
+    const user = await this.userRepository.save({
+      name: data.name,
+      email: data.email,
+      password: hashedPassword,
+      bio: data.bio
+    })
 
     return success({ user })
+  }
+
+  async findOneByEmail(email: string): Promise<User | null> {
+    const user = await this.userRepository.findByEmail(email)
+
+    return user
   }
 }
