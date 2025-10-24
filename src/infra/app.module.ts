519fa3479ff600158d3a70eabbb93ff36ff9c9c1
Murilo Damario <murilo.gcdamario@gmail.com>
2025-10-23 21:57:34 -0300

    :sparkles: feat: Implements Env configuration

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true
+    }),
+    EnvModule,
+  ],
   providers: [],
 })
 export class AppModule { }
--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
+
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3333);
+  const envService = app.get(EnvService)
+
+  const port = envService.get('PORT')
+
+  await app.listen(port)
 }
 bootstrap();
