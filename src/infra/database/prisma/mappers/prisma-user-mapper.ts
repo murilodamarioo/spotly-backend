ff8e471d4d979041f1696927db4cdeaead0591a8
Murilo Damario <murilo.gcdamario@gmail.com>
2025-10-25 16:36:42 -0300

    :sparkles: feat: Prisma User repository and Mapper

---
+++ src/infra/database/prisma/mappers/prisma-user-mapper.ts
@@ -0,0 +1,25 @@
+import { Prisma, User as PrismaUser } from '@prisma/client'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { User } from '@/domain/core/enterprise/entities/user'
+
+export class PrismaUserMapper {
+  
+  static toDomain(raw: PrismaUser): User {
+    return User.create({
+      name: raw.name,
+      email: raw.email,
+      password: raw.password,
+      bio: raw.bio
+    }, new UniqueEntityId(raw.id))
+  }
+
+  static toPrisma(user: User): Prisma.UserUncheckedCreateInput {
+    return {
+      id: user.id.toString(),
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      bio: user.bio
+    }
+  }
+}
---
+++ src/infra/database/prisma/repositpries/prisma-user-repository.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common'
+
+import { PrismaService } from '../prisma.service';
+
+import { UsersRepository } from '@/domain/core/application/repositories/users-repository'
+import { User } from '@/domain/core/enterprise/entities/user'
+
+import { PrismaUserMapper } from '../mappers/prisma-user-mapper'
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+
+  constructor(private prisma: PrismaService) { }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { id }
+    })
+
+    return user ? PrismaUserMapper.toDomain(user) : null
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { email }
+    })
+
+    return user ? PrismaUserMapper.toDomain(user) : null
+  }
+
+  async create(user: User): Promise<void> {
+    const data = PrismaUserMapper.toPrisma(user)
+
+    await this.prisma.user.create({ data })
+  }
+
+  async save(user: User): Promise<void> {
+    const data = PrismaUserMapper.toPrisma(user)
+
+    await this.prisma.user.update({
+      where: { id: data.id },
+      data
+    })
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.prisma.user.delete({
+      where: { id }
+    })
+  }
+
+}
