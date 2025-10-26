3357d37418f4db014cf776cbb431e693d5a6998e
Murilo Damario <murilo.gcdamario@gmail.com>
2025-10-26 16:23:34 -0300

    :recycle: refactor: Adds zod validation pipe

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { BadRequestException, Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
 import z from 'zod'
 
 import { AuthenticateUserUseCase } from '@/domain/core/application/use-cases/authenticate-user'
@@ -6,6 +6,7 @@ import { AuthenticateUserUseCase } from '@/domain/core/application/use-cases/aut
 import { Public } from '@/infra/auth/public'
 import { InvalidCredentialsError } from '@/core/errors/errors-message'
 import { ApiBadRequestResponse, ApiBody, ApiOkResponse, ApiTags } from '@nestjs/swagger'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
 
 const authenticateUserBodySchema = z.object({
   email: z.email(),
@@ -23,6 +24,7 @@ export class AuthenticateController {
 
   @Post()
   @HttpCode(200)
+  @UsePipes(new ZodValidationPipe(authenticateUserBodySchema))
   @ApiBody({
     schema: {
       type: 'object',
