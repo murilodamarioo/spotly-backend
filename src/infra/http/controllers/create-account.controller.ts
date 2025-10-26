22980dc51aa2fdff870127e451e1fd2fb05c602d
Murilo Damario <murilo.gcdamario@gmail.com>
2025-10-25 22:49:01 -0300

    :books: docs: Swagger create-account controller

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Body, ConflictException, Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
+import { ApiBody, ApiConflictResponse, ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
 
 import { z } from 'zod'
 
@@ -17,6 +18,7 @@ const createAccountBodySchema = z.object({
 
 type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
+@ApiTags('accounts')
 @Controller('/sign-up')
 export class CreateAccountController {
 
@@ -25,6 +27,27 @@ export class CreateAccountController {
   @Post()
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  @ApiBody({
+    schema: {
+      properties: {
+        name: { type: 'string', example: 'John Wick' },
+        email: { type: 'string', example: 'john@gmail.com' },
+        password: { type: 'string', example: 'nEwP@ssw0rd' },
+        bio: { type: 'string', example: 'I am John Wick, the best hitman in the world.' }
+      },
+      required: ['name', 'email', 'password']
+    }
+  })
+  @ApiCreatedResponse({ description: 'Account created successfully' })
+  @ApiConflictResponse({
+    schema: {
+      properties: {
+        message: { type: 'string', example: 'User already exists with the same e-mail' },
+        error: { type: 'string', example: 'Conflict' },
+        statusCode: { type: 'number', example: 409 },
+      }
+    }
+  })
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password, bio } = body
 
