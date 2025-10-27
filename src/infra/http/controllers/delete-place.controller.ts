5cd972974a7abeecd6c3e2b737f1b84d2d2ea3dc
Murilo Damario <murilo.gcdamario@gmail.com>
2025-10-27 16:15:22 -0300

    :books: docs: Swagger delete-place controller

--- src/infra/http/controllers/delete-place.controller.ts
+++ src/infra/http/controllers/delete-place.controller.ts
@@ -1,5 +1,7 @@
 import { BadRequestException, Controller, Delete, ForbiddenException, HttpCode, Param } from '@nestjs/common'
 
+import { ApiBearerAuth, ApiForbiddenResponse, ApiNoContentResponse, ApiParam, ApiTags } from '@nestjs/swagger'
+
 import { NotAllowedError } from '@/core/errors/errors-message'
 
 import { DeletePlaceUseCase } from '@/domain/core/application/use-cases/delete-place'
@@ -7,6 +9,8 @@ import { DeletePlaceUseCase } from '@/domain/core/application/use-cases/delete-p
 import { CurrentUser } from '@/infra/auth/current-user-decorator'
 import type { UserPayload } from '@/infra/auth/jwt.strategy'
 
+@ApiTags('places')
+@ApiBearerAuth('jwt')
 @Controller('/places/:id')
 export class DeletePlaceController {
 
@@ -14,6 +18,22 @@ export class DeletePlaceController {
 
   @Delete()
   @HttpCode(204)
+  @ApiParam({
+    name: 'id',
+    type: 'uuid',
+    example: 'a0b1c2d3-e4f5-6g7h-8i9j-0k1l2m3n4o'
+  })
+  @ApiNoContentResponse({ description: 'Place deleted successfully' })
+  @ApiForbiddenResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        message: { type: 'string', example: 'Not allowed to perform this action.' },
+        error: { type: 'string', example: 'Forbidden' },
+        statusCode: { type: 'number', example: 403 }
+      }
+    }
+  })
   async handle(@CurrentUser() user: UserPayload, @Param('id') placeId: string) {
     const userId = user.sub
 
