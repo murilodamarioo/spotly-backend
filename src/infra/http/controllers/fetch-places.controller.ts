d5427b966012bc16f425afc1bf89ffa3a342a3fc
Murilo Damario <murilo.gcdamario@gmail.com>
2025-10-30 23:56:04 -0300

    :books: docs: Swagger fetch-places controller

--- src/infra/http/controllers/fetch-places.controller.ts
+++ src/infra/http/controllers/fetch-places.controller.ts
@@ -5,6 +5,7 @@ import z from 'zod'
 import { FetchPlacesUseCase } from '@/domain/core/application/use-cases/fetch-places'
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
 import { FetchPlacesPresenter } from '@/infra/presenters/fetch-places-presenter'
+import { ApiBearerAuth, ApiOkResponse, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger'
 
 const pageQueryParamSchema = z
   .string()
@@ -17,6 +18,8 @@ type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 
 const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
 
+@ApiTags('places')
+@ApiBearerAuth('jwt')
 @Controller('/places')
 export class FetchPlacesController {
 
@@ -24,6 +27,37 @@ export class FetchPlacesController {
 
   @Get()
   @HttpCode(200)
+  @ApiQuery({
+    name: 'page',
+    type: 'number',
+    required: true,
+  })
+  @ApiOkResponse({
+    schema: {
+      example: {
+        places: [
+          {
+            name: 'Coffee Shop',
+            category: 'Restaurant',
+            description: 'A cozy coffee shop in downtown.',
+            attachments: [
+              'attachment1.jpg',
+              'attachment2.jpg'
+            ]
+          },
+          {
+            name: 'Bart`s Pub',
+            category: 'Pub',
+            description: 'N/A',
+            attachments: [
+              'attachment1.jpg',
+              'attachment2.jpg'
+            ]
+          }
+        ]
+      }
+    }
+  })
   async handle(
     @Query('page', queryValidationPipe) page: PageQueryParamSchema
   ) {
