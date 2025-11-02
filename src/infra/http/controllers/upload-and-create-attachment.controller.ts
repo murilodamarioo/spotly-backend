b9bf0012b1a02043e36d231ddee51fac58b46bfa
Murilo Damario <murilo.gcdamario@gmail.com>
2025-11-02 17:42:14 -0300

    :books: docs: Swagger upload-attachment controller

--- src/infra/http/controllers/upload-and-create-attachment.controller.ts
+++ src/infra/http/controllers/upload-and-create-attachment.controller.ts
@@ -3,12 +3,13 @@ import {
   Controller,
   FileTypeValidator,
   HttpCode,
-  MaxFileSizeValidator, 
-  ParseFilePipe, 
-  Post, 
-  UploadedFile, 
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
   UseInterceptors
 } from '@nestjs/common'
+import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiConsumes, ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
 import { FileInterceptor } from '@nestjs/platform-express'
 
 import { InvalidAttachementTypeError } from '@/core/errors/errors-message'
@@ -26,6 +27,8 @@ const parseFilePipe = new ParseFilePipe({
   ]
 })
 
+@ApiTags('attachmemts')
+@ApiBearerAuth('jwt')
 @Controller('/attachments')
 export class UploadAndCreateAttachmentController {
 
@@ -34,6 +37,37 @@ export class UploadAndCreateAttachmentController {
   @Post()
   @HttpCode(201)
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        }
+      },
+      required: ['file']
+    }
+  })
+  @ApiCreatedResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        attachmentId: { type: 'uuid', example: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d' }
+      }
+    }
+  })
+  @ApiBadRequestResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        message: { type: 'string', example: 'Validation failed (current file type is image/gif, expected type is .(png|jpeg|jpg))' },
+        error: { type: 'string', example: 'Bad Request' },
+        statusCode: { type: 'number', example: 400 }
+      }
+    }
+  })
   async handle(@UploadedFile(parseFilePipe) file: Express.Multer.File) {
 
     const response = await this.uploadAndCreateAttachment.execute({
