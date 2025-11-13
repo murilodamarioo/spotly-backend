9beb391e6c0a71c6fcd32d9df780569aa6bb6f02
Murilo Damario <murilo.gcdamario@gmail.com>
2025-11-12 23:53:48 -0300

    :books: docs: Update Swagger get-place

--- src/infra/http/controllers/get-place.controller.ts
+++ src/infra/http/controllers/get-place.controller.ts
@@ -8,6 +8,15 @@ import {
   Param
 } from '@nestjs/common'
 
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags
+} from '@nestjs/swagger'
+
 import { NotAllowedError, ResourceNotFoundError } from '@/core/errors/errors-message'
 
 import { GetPlaceUseCase } from '@/domain/core/application/use-cases/get-place'
@@ -16,6 +25,8 @@ import { CurrentUser } from '@/infra/auth/current-user-decorator'
 import type { UserPayload } from '@/infra/auth/jwt.strategy'
 import { PlacePresenter } from '@/infra/presenters/place-presenter'
 
+@ApiTags('places')
+@ApiBearerAuth('jwt')
 @Controller('/places/:id')
 export class GetPlaceController {
 
@@ -23,6 +34,49 @@ export class GetPlaceController {
 
   @Get()
   @HttpCode(200)
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    example: 'c4b5b4b5-b4b5-b4b5-b4b5-b4b5b4b5b4b5'
+  })
+  @ApiOkResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        place: {
+          properties: {
+            id: { type: 'string', example: 'c4b5b4b5-b4b5-b4b5-b4b5-b4b5b4b5b4b5' },
+            name: { type: 'string', example: 'Place name' },
+            category: { type: 'string', example: 'Category' },
+            description: { type: 'string', example: 'Description' },
+            address: { type: 'string', example: 'Address' },
+            city: { type: 'string', example: 'City' },
+            state: { type: 'string', example: 'State' },
+            createdAt: { type: 'string', example: '2023-01-01T00:00:00.000Z' },
+            updatedAt: { type: 'string', example: '2023-01-01T00:00:00.000Z' }
+          }
+        }
+      }
+    }
+  })
+  @ApiNotFoundResponse({
+    schema: {
+      properties: {
+        message: { type: 'string', example: 'Resource not found' },
+        error: { type: 'string', example: 'Not Found' },
+        statusCode: { type: 'number', example: 404 }
+      }
+    }
+  })
+  @ApiForbiddenResponse({
+    schema: {
+      properties: {
+        message: { type: 'string', example: 'Not allowed to perform this action.' },
+        error: { type: 'string', example: 'Forbidden' },
+        statusCode: { type: 'number', example: 403 }
+      }
+    }
+  })
   async handle(
     @Param('id') placeId: string,
     @CurrentUser() user: UserPayload
