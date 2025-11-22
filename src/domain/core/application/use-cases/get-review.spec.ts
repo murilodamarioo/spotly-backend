aa808780b8fcc8a61fced049ec53e4512249a953
Murilo Damario <murilo.gcdamario@gmail.com>
2025-11-22 09:54:10 -0300

    :test_tube: test: refactor get-review test

--- src/domain/core/application/use-cases/get-review.spec.ts
+++ src/domain/core/application/use-cases/get-review.spec.ts
@@ -4,6 +4,7 @@ import { InMemoryReviewsRepository } from 'test/repositories/in-memory-reviews-r
 import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
 import { InMemoryReviewAttachmentsRepository } from 'test/repositories/in-memory-review-attachments-repository'
 import { makeReview } from 'test/factories/make-review'
+import { makeUser } from 'test/factories/make-user'
 
 let inMemoryReviewsRepository: InMemoryReviewsRepository
 let inMemoryUsersRepository: InMemoryUsersRepository
@@ -23,7 +24,12 @@ describe('Get Review', () => {
   })
 
   it('should be able to get a review with valid id', async () => {
-    const review = makeReview()
+    const reviewer = makeUser()
+    await inMemoryUsersRepository.create(reviewer)
+
+    const review = makeReview({
+      reviewerId: reviewer.id
+    })
     await inMemoryReviewsRepository.create(review)
 
     const response = await sut.execute({
@@ -31,6 +37,11 @@ describe('Get Review', () => {
     })
 
     expect(response.isSuccess()).toBeTruthy()
-    expect(response.value).toMatchObject({ review })
+    expect(response.value).toMatchObject({
+      review: expect.objectContaining({
+        rating: review.rating,
+        comment: review.comment
+      })
+    })
   })
 })
