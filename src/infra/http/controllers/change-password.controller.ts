3852d3c0708de0f1bfe9cca6d76fdeec3d10f4c6
Murilo Damario <murilo.gcdamario@gmail.com>
2025-12-15 20:02:43 -0300

    :books: docs: Swagger for change-password documentation

--- src/infra/http/controllers/change-password.controller.ts
+++ src/infra/http/controllers/change-password.controller.ts
@@ -1,13 +1,32 @@
-import { BadRequestException, Body, Controller, HttpCode, NotFoundException, Patch } from '@nestjs/common'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  NotFoundException,
+  Patch
+} from '@nestjs/common'
+
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiTags
+} from '@nestjs/swagger'
 
 import z from 'zod'
 
+import { InvalidPasswordError } from '@/core/errors/errors-message/invalid-password'
+import { ResourceNotFoundError } from '@/core/errors/errors-message'
+
 import { ChangePasswordUseCase } from '@/domain/core/application/use-cases/change-password'
+
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+
 import { CurrentUser } from '@/infra/auth/current-user-decorator'
 import type { UserPayload } from '@/infra/auth/jwt.strategy'
-import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
-import { ResourceNotFoundError } from '@/core/errors/errors-message'
-import { InvalidPasswordError } from '@/core/errors/errors-message/invalid-password'
 
 const changePasswordBodySchema = z.object({
   oldPassword: z.string(),
@@ -16,6 +35,8 @@ const changePasswordBodySchema = z.object({
 
 type ChangePasswordBodySchema = z.infer<typeof changePasswordBodySchema>
 
+@ApiTags('users')
+@ApiBearerAuth('jwt')
 @Controller('/users/me/change-password')
 export class ChangePasswordController {
 
@@ -23,6 +44,34 @@ export class ChangePasswordController {
 
   @Patch()
   @HttpCode(204)
+  @ApiBody({
+    schema: {
+      properties: {
+        oldPassword: { type: 'string', example: '0ldP@Ssw0rd' },
+        newPassword: { type: 'string', example: 'NewP@Ssw0rd' }
+      }
+    }
+  })
+  @ApiNoContentResponse({ description: 'Password changed' })
+  @ApiBadRequestResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        message: { type: 'string', example: 'Old password does not match with the current password' },
+        error: { type: 'string', example: 'Bad Request' },
+        statusCode: { type: 'number', example: 400 }
+      }
+    }
+  })
+  @ApiNotFoundResponse({
+    schema: {
+      properties: {
+        message: { type: 'string', example: 'Resource not found.' },
+        error: { type: 'string', example: 'Not Found' },
+        statusCode: { type: 'number', example: 404 }
+      }
+    }
+  })
   async hadle(
     @CurrentUser() user: UserPayload,
     @Body(new ZodValidationPipe(changePasswordBodySchema)) body: ChangePasswordBodySchema
