f3368a49e6088956a7fb4dac3ae943340393aa1c
Murilo Damario <murilo.gcdamario@gmail.com>
2025-12-16 19:50:21 -0300

    :sparkles: feat: Create password-reset-token entity

---
+++ src/domain/core/enterprise/entities/password-reset-token.ts
@@ -0,0 +1,45 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+interface PasswordResetTokenProps {
+  userId: UniqueEntityId
+  token: string
+  expiresAt: Date
+  createdAt: Date
+}
+
+export class PasswordResetToken extends Entity<PasswordResetTokenProps> {
+
+  get userId() {
+    return this.props.userId
+  }
+
+  get token() {
+    return this.props.token
+  }
+
+  get expiresAt() {
+    return this.props.expiresAt
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  public isValid() {
+    return new Date() < this.props.expiresAt
+  }
+
+  static create(
+    props: Optional<PasswordResetTokenProps, 'createdAt'>,
+    id?: UniqueEntityId
+  ): PasswordResetToken {
+    const passwordResetToken = new PasswordResetToken({
+      ...props,
+      createdAt: props.createdAt ?? new Date()
+    }, id)
+
+    return passwordResetToken
+  }
+}
