873493fedd36c78cb005cf86af219440afece924
Murilo Damario <murilo.gcdamario@gmail.com>
2025-12-21 16:39:02 -0300

    :sparkles: feat: Implment decrypt method

--- src/infra/cryptography/jwt-encrypter.ts
+++ src/infra/cryptography/jwt-encrypter.ts
@@ -1,14 +1,19 @@
-import { Encrypter } from '@/domain/core/application/cryptography'
 import { Injectable } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
+import { JwtService, JwtSignOptions } from '@nestjs/jwt'
+
+import { Encrypter } from '@/domain/core/application/cryptography'
 
 @Injectable()
 export class JwtEncrypter implements Encrypter {
 
   constructor(private jwtService: JwtService) { }
 
-  encrypt(payload: Record<string, unknown>): Promise<string> {
-    return this.jwtService.signAsync(payload)
+  encrypt(payload: Record<string, unknown>, options?: { expiresIn?: string | number }): Promise<string> {
+    return this.jwtService.signAsync(payload, options as JwtSignOptions)
+  }
+
+  decrypt(token: string): Promise<Record<string, unknown>> {
+    return this.jwtService.verifyAsync(token)
   }
 
 }
